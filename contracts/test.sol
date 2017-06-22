b4f4c5171d7ddfa7f0637e9a8cd0d6e7f487796f
jjcool <jjjayesh26@gmail.com>
2017-06-22 18:06:31 +0530

    Log added
    to contract

--- contracts/test.sol
+++ contracts/test.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.0 ;
 
 contract test{
 	
-	Request[] public policies;
+	policy[] public policies;
 
 	uint constant ShaLength = 40 ; 
 
@@ -12,26 +12,62 @@ contract test{
 		quotation ,
 		order_complete
 	}
+	
+	event LOG_PolicyApplied(
+	    uint policyId,
+	 	address customer,
+	 	string referenceId,
+	 	uint premium,
+	 	uint timeStamp
+	    );
 
-	struct Request{
+	struct policy{
 		uint id;
+		address customer;
 		string reference_id;
 		string carrier ; 
 		string doc_url;
-		uint createdAt;
+		uint timeStamp;
 		uint premium ; 
+		string status ; 
 	}
 	 mapping (address => uint[]) public customerPolicies;
+	 mapping (uint => uint) public counter ; 
+
 
 	function new_request(string reference_id, string carrier , uint premium) returns (bool) {
 
-		Request memory p ;
+		uint policyId = policies.length++;
+        customerPolicies[this].push(policyId);
+        policy p = policies[policyId];
+        
+        p.customer = this ;
+        p.timeStamp = now ;
         p.reference_id = reference_id ; 
         p.carrier = carrier ; 
         p.premium = premium ; 
-
+        p.status = "To be approved " ;
+        counter[policyId] = 0 ;
+        
+        LOG_PolicyApplied(policyId , this , reference_id , p.premium , p.timeStamp);
+        
         return true ; 
+        
+       
 
+	}
+    // uint k  = 100 ;
+	function counters(uint _policyId) returns (string) {
+
+		
+		counter[_policyId] = counter[_policyId]+  1; 
+		policy x = policies[_policyId] ;
+
+		if (counter[_policyId]  > 3 ){
+			x.status = "approved";
+		} 
+
+		return x.status ;
 	}
 
 
