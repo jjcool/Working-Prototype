039a9bb80e93750c559ed18951baafcb47c92ac3
jjcool <jjjayesh26@gmail.com>
2017-06-26 17:37:30 +0530

    Claim Done
    Now UI and security to be added

--- App/public/javascripts/main.js
+++ App/public/javascripts/main.js
@@ -1,9 +1,9 @@
 
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"customerPolicies","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_policyId","type":"uint256"}],"name":"status","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"counter","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"policies","outputs":[{"name":"id","type":"uint256"},{"name":"customer","type":"address"},{"name":"reference_id","type":"string"},{"name":"carrier","type":"string"},{"name":"doc_url","type":"string"},{"name":"timeStamp","type":"uint256"},{"name":"premium","type":"uint256"},{"name":"status","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"reference_id","type":"string"},{"name":"carrier","type":"string"},{"name":"premium","type":"uint256"}],"name":"new_request","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_policyId","type":"uint256"}],"name":"counters","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"policyId","type":"uint256"},{"indexed":false,"name":"customer","type":"address"},{"indexed":false,"name":"referenceId","type":"string"},{"indexed":false,"name":"premium","type":"uint256"},{"indexed":false,"name":"timeStamp","type":"uint256"},{"indexed":false,"name":"status","type":"string"}],"name":"LOG_PolicyApplied","type":"event"}]')
+abi = JSON.parse('[{"constant":false,"inputs":[{"name":"addresss","type":"address"},{"name":"id","type":"uint256"},{"name":"premium","type":"uint256"}],"name":"new_claim","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"counterClaim","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"customerPolicies","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"customerClaims","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"counterPolicy","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_claimId","type":"uint256"}],"name":"counterClaims","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_policyId","type":"uint256"}],"name":"counterPolicys","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"claims","outputs":[{"name":"id","type":"uint256"},{"name":"customer","type":"address"},{"name":"reference_id","type":"string"},{"name":"carrier","type":"string"},{"name":"doc_url","type":"string"},{"name":"timeStamp","type":"uint256"},{"name":"premium","type":"uint256"},{"name":"status","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"policies","outputs":[{"name":"id","type":"uint256"},{"name":"customer","type":"address"},{"name":"reference_id","type":"string"},{"name":"carrier","type":"string"},{"name":"doc_url","type":"string"},{"name":"timeStamp","type":"uint256"},{"name":"premium","type":"uint256"},{"name":"status","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"reference_id","type":"string"},{"name":"carrier","type":"string"},{"name":"premium","type":"uint256"}],"name":"new_request","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_policyId","type":"uint256"}],"name":"statusPolicy","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_claimId","type":"uint256"}],"name":"statusClaim","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"policyId","type":"uint256"},{"indexed":false,"name":"customer","type":"address"},{"indexed":false,"name":"referenceId","type":"string"},{"indexed":false,"name":"premium","type":"uint256"},{"indexed":false,"name":"timeStamp","type":"uint256"},{"indexed":false,"name":"status","type":"string"}],"name":"LOG_PolicyApplied","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"policyId","type":"uint256"},{"indexed":false,"name":"customer","type":"address"},{"indexed":false,"name":"id","type":"uint256"},{"indexed":false,"name":"premium","type":"uint256"},{"indexed":false,"name":"timeStamp","type":"uint256"},{"indexed":false,"name":"status","type":"string"}],"name":"LOG_ClaimApplied","type":"event"}]')
 VotingContract = web3.eth.contract(abi);
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = VotingContract.at('0x57d53f52fd80a15eb59f467294fb96c6a7b2a8c7');
+contractInstance = VotingContract.at('0x77599855cf2a375190fbd0c05fef26a417960a25');
 //candidates = {"Jayesh": "candidate-1", "Pankaj": "candidate-2", "Jose": "candidate-3"}
 
 var txFilter = contractInstance.LOG_PolicyApplied();
@@ -48,22 +48,66 @@ txFilter.watch(function(err , result){
 })
 }
 
-function vote(policyId, voterId){
+function votePolicy(policyId, voterId){
 	policyId = $('#policyId').val();
 	voterId = $('#voterId').val();
 	if(voterId == "Saurabh" || voterId == "Pankaj" || voterId == "Jayesh" || voterId == "Parmod"){
 	console.log("started");
-	contractInstance.counters(policyId , {from: web3.eth.accounts[0] , gas :'900000'});
+	contractInstance.counterPolicys(policyId , {from: web3.eth.accounts[0] , gas :'900000'});
 	console.log("ended");
 	}
 	else alert("Wrong VoterID");
 }
 
-function status(policyId1){
+function statusPolicy(policyId1){
 	policyId1 = $('#policyId1').val();
-	var count = 0 ;
-	alert(contractInstance.status(policyId1));
+	
+	alert(contractInstance.statusPolicy(policyId1));
 
+}
+
+function newClaim(address , id , premium ) {
+   address = $("#address").val();
+  id = $("#id").val();
+  premium = $("#premium").val();
+
+  //if(address != "" && id != "" && premium != "" ){
+ 
+console.log("Claim started");
+contractInstance.new_claim(address,id,premium , {from: web3.eth.accounts[0] , gas :'900000'})/*, function() {
+    let div_id = candidates[candidateName];
+    $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
+  }*/;
+
+//$('#table').append('<tr><th text = "ref_id"></th><td>more data</td><td>mosre data</td></tr>');
+
+console.log("Claim made");
+}
+
+//else { console.log("wrong") }
+//}
+
+
+function voteClaim(claimId, voterId){
+	claimId = $('#claimId').val();
+	voterId = $('#voterId').val();
+	//if(voterId == "Saurabh" || voterId == "Pankaj" || voterId == "Jayesh" || voterId == "Parmod"){
+	console.log("started");
+	contractInstance.counterClaims(claimId , {from: web3.eth.accounts[0] , gas :'900000'});
+	console.log("ended");
+	//}
+	//else alert("Wrong VoterID");
+}
+
+function statusClaim(claimId1){
+	claimId1 = $('#claimId1').val();
+	if(contractInstance.statusClaim(claimId1) == "approved") {
+		console.log(web3.eth.getBalance(web3.eth.accounts[1]).toNumber(),"ether");
+		web3.eth.sendTransaction({from:web3.eth.coinbase, to:web3.eth.accounts[1], value: web3.toWei(0.005, "ether")});
+		console.log("money sent");
+		console.log(web3.eth.getBalance(web3.eth.accounts[0]).toNumber());
+	}
+	else alert(contractInstance.statusClaim(claimId1));
 }
 
 // $(document).ready(function(){
--- contracts/test.sol
+++ contracts/test.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.0 ;
 contract test{
 	
 	policy[] public policies;
+	policy[] public claims ;
 
 	uint constant ShaLength = 40 ; 
 
@@ -21,6 +22,16 @@ contract test{
 	 	uint timeStamp,
 	 	string status
 	    );
+	    
+	event LOG_ClaimApplied(
+	    uint policyId,
+	 	address customer,
+	 	uint id ,
+	 	uint premium,
+	 	uint timeStamp,
+	 	string status
+	    );
+
 
 	struct policy{
 		uint id;
@@ -33,7 +44,10 @@ contract test{
 		string status ; 
 	}
 	 mapping (address => uint[]) public customerPolicies;
-	 mapping (uint => uint) public counter ; 
+	 mapping (address => uint[]) public customerClaims;
+	 mapping (uint => uint) public counterPolicy ; 
+     mapping (uint => uint) public counterClaim ; 
+
 
 
 	function new_request(string reference_id, string carrier , uint premium) returns (bool) {
@@ -48,7 +62,7 @@ contract test{
         p.carrier = carrier ; 
         p.premium = premium ; 
         p.status = "To be approved " ;
-        counter[policyId] = 0 ;
+        counterPolicy[policyId] = 0 ;
         
         LOG_PolicyApplied(policyId , this , reference_id , p.premium , p.timeStamp,p.status);
         
@@ -57,12 +71,44 @@ contract test{
        
 
 	}
+	
+	function new_claim(address addresss, uint id, uint premium) returns (bool) {
+
+		uint claimId = claims.length++;
+        customerClaims[this].push(claimId);
+        policy p = claims[claimId];
+        
+        p.customer = addresss ;
+        p.timeStamp = now ;
+        p.id = id ;
+        p.premium = premium ; 
+        p.status = "To be approved" ;
+        counterClaim[claimId] = 0 ;
+        
+        LOG_ClaimApplied(claimId ,p.customer,id , p.premium , p.timeStamp,p.status);
+        
+        return true ; 
+        
+       
+
+	}
     // uint k  = 100 ;
-	function counters(uint _policyId) returns (string) {
-		counter[_policyId] = counter[_policyId]+  1; 
+	function counterPolicys(uint _policyId) returns (string) {
+		counterPolicy[_policyId] = counterPolicy[_policyId]+  1; 
 		policy x = policies[_policyId] ;
 
-		if (counter[_policyId]  > 3 ){
+		if (counterPolicy[_policyId]  > 3 ){
+			x.status = "approved";
+		} 
+
+		return x.status ;
+	}
+	
+	function counterClaims(uint _claimId) returns (string) {
+		counterClaim[_claimId] = counterClaim[_claimId]+  1; 
+		policy x = claims[_claimId] ;
+
+		if (counterClaim[_claimId]  > 3 ){
 			x.status = "approved";
 		} 
 
@@ -70,11 +116,18 @@ contract test{
 	}
 	
 	
-	function status (uint _policyId) constant returns (string) {
+	function statusPolicy (uint _policyId) constant returns (string) {
 		
 		policy x = policies[_policyId] ;
 		return x.status ;
 	}
+
+	function statusClaim (uint _claimId) constant returns (string) {
+		
+		policy x = claims[_claimId] ;
+		return x.status ;
+	}
+
 
 
 }
